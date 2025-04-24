0d71ae91ca9cec32c09e922ea458cd55c4ec190d
premskumar02 <premskumar2002@gmail.com>
2025-04-24 12:14:39 +0530

    Add files via upload

---
+++ src/test/java/prem/Apptest.java
@@ -0,0 +1,25 @@
+package prem;
+
+import org.testng.Assert; 
+import org.testng.annotations. Test; 
+
+ 
+public class Apptest {                                            
+ 
+	@Test 
+
+	public void testlogin1() 
+	{ 
+		App myapp=new App(); 
+		Assert.assertEquals(0, myapp.userLogin("abc", "abc123")); 
+	} 
+
+	@Test 
+	
+	public void testlogin2() 
+	{ 
+		App myapp=new App(); 
+		Assert.assertEquals(1, myapp.userLogin("abc", "abc@123")); 
+	}  
+} 
+ 
